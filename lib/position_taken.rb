fc0aeef9932301c464334af068a14afe67950351
Corey Norton <fatcatcooldude@aol.com>
2020-05-07 15:37:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken(board, index)
+  if index = " " || ""
+    return false
+  elsif index = nil
+    return false
+  else
+    return true
+  end
+
+  position_taken(board, "X")
+    
