b44de203b10dd31ad68356fda53eb142332b2fe9
Corey Norton <fatcatcooldude@aol.com>
2020-05-07 16:22:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
     return false
   elsif index == nil
     return false
-  else index = "X" || "O"
+  else index != " " || "" || nil
       return true
   end
 end
